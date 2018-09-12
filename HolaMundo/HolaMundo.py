e71230e43ad11d4c97c819c275bba0d64cf629f8
Juan Dario Rodas <jdrodas@hotmail.com>
2018-09-12 11:47:45 -0500

    Agregado el proyecto HolaMundo

---
+++ HolaMundo/HolaMundo.py
@@ -0,0 +1,28 @@
+# ============================
+#Programa:       HolaMundo
+#Contacto:       Juan Dario Rodas - jdrodas@hotmail.com
+
+#Propósito:
+#----------
+# - Demostrar el funcionamiento básico del Entorno Integrado de Desarrollo (IDE).
+# - Visualizar información en la consola, en forma de cadena de caracteres.
+# - Declarar variables asignándo valores string y visualizar su contenido en la consola.
+# - Demostrar el funcionamiento de la función print().
+
+# ============================
+
+# Comenzamos con la forma más básica de colocar información en consola.
+# Utilizamos la función print()
+# Escribe una cadena de caracteres, delimitada por comillas simples
+
+print('Hola Mundo!')
+
+# Aqui utilizamos el caracter especial \n para agregar saltos de linea
+print('\nEsta frase se escribe en una linea \ny esta en otra\n')
+
+# Aqui utilizamos el caracter especial \t para separar en una misma con tabuladores
+print("Primera parte \t Segunda Parte \t Tercera Parte")
+
+# Aqui declaramos una variable y le asignamos el valor inicial
+unaFrase = '\nEsta es una frase almacenada en una variable'
+print(unaFrase)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python_funprog
 Proyectos del curso de Fundamentos de Programación en Python
+
+## Hola Mundo
+- Visualizar información en la consola, en forma de cadena de caracteres.
+- Declarar variables tipo string y visualizar su contenido en la consola.
+- Demostrar el funcionamiento de la función print()
